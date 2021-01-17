83e6479dda39d243df395ea62797da99776043c8
Emiel de Jong <emieldejong@xs4all.nl>
2021-01-17 09:52:28 +0100

    Initial commit

---
+++ src/main/java/nl/saxofoonleren/springsecurityauthenticationjavabrains/SpringSecurityAuthenticationJavabrainsApplication.java
@@ -0,0 +1,13 @@
+package nl.saxofoonleren.springsecurityauthenticationjavabrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthenticationJavabrainsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityAuthenticationJavabrainsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/saxofoonleren/springsecurityauthenticationjavabrains/SpringSecurityAuthenticationJavabrainsApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.saxofoonleren.springsecurityauthenticationjavabrains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAuthenticationJavabrainsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
